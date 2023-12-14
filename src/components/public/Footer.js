f1da9b6f4f2a76a0a026ddf88edcca43fd00b262
Miguel borja <miguelangelborjagallego@gmail.com>
2023-12-14 00:37:56 -0500

    firstList

---
+++ src/components/public/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p className="col-md-4 mb-0 text-body-secondary">&copy; 2023 Company, Inc</p>
+
+    <span to={"/"} className='col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none'>
+      <img className='App-logo' height="30"
+      src='cat.png' alt='gato-giratorio'>
+      </img>   
+    </span>
+
+    <ul className="nav col-md-4 justify-content-end">
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+  </footer>
+    </>
+  )
+}
+
+export default Footer; 
